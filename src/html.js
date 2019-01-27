03effd266654670b8a3cd0f61c17ca474e8b3a87
TheodorosPloumis <me@theodorosploumis.com>
2019-01-27 03:35:34 +0200

    Add school link on footer

--- src/html.js
+++ src/html.js
@@ -24,7 +24,7 @@ export default class HTML extends React.Component {
           {this.props.postBodyComponents}
           <footer className="container">
             <hr/>
-            <p>Copyright &copy; 2019, All rights reserved | <a href="https://github.com/enato/parents" tagret="_blank">GitHub source</a></p>
+            <p>Copyright &copy; 2019, All rights reserved | <a href="https://github.com/enato/parents" tagret="_blank">GitHub source</a> | <a href="https://www.enato.edu.gr" tagret="_blank">Ιστότοπος Σχολείου</a></p>
           </footer>
         </body>
       </html>
