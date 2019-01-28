5d05f2f62274014c7644b1e843bf924099e35574
TheodorosPloumis <me@theodorosploumis.com>
2019-01-28 12:12:07 +0200

    Add new pages

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -5,5 +5,6 @@ import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerPreviewTemplate('priorities', AboutPagePreview)
 CMS.registerPreviewTemplate('sponsors', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
